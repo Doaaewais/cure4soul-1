ac4c98504eab46207691d95fffbe85905b662d75
Habibaaliyoussef <habibaaliyoussef237@gmail.com>
2023-05-07 18:30:23 +0300

    mm

--- login project/requesttherapist.php
+++ login project/requesttherapist.php
@@ -15,7 +15,8 @@ if(isset($_post['submit'])){
   $phone=mysqli_real_escape_string($conn,$_post['phone']);
   $nid=mysqli_real_escape_string($conn,$_post['nid']);
   $cv=mysqli_real_escape_string($conn,$_post['cv']);
-  $sql="INSERT INTO reqther(fname,mname,lname,dayy,monthh,yearr,addresss,email,spec,phone,nid,cv) values ('$fname','$mname','$lname','$dayy','$monthh','$yearr','$addresss','$email','$spec','$phone','$nid','$cv')";
+
+  $sql="INSERT INTO reqther(fname,mname,lname,dayy,monthh,yearr,addresss,email,spec,phone,nid,cv) Values('$fname','$mname','$lname','$dayy','$monthh','$yearr','$addresss','$email','$spec','$phone','$nid','$cv')";
   mysqli_query($conn,$sql);
   
 }

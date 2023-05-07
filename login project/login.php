f5bf1a748042ab92e1eeadbecd4edad6e4865bfc
Habibaaliyoussef <habibaaliyoussef237@gmail.com>
2023-05-07 18:05:51 +0300

    mm

--- login project/login.php
+++ login project/login.php
@@ -1,3 +1,20 @@
+<?php include("connection.php"); ?>
+<?php
+if(! isset($_session)){
+    session_start();
+}
+if(isset($_POST['login'])){
+    $n=mysqli_real_escape_string($conn,$_POST['company']);
+    $e=mysqli_real_escape_string($conn,$_POST['email']);
+    $p=mysqli_real_escape_string($conn,$_POST['password']);
+    $t="SELECT * FROM emp WHERE 'company'='$n' AND 'email'='$e' AND 'password'='$p'";
+    $r=mysqli_query($conn,$t); 
+if(mysqli_num_rows($r)==1){
+    header('location:requestdemo.php');
+}    
+}
+
+?>
 <html>
 
 <head>
@@ -13,7 +30,7 @@
         Login to CURE4SOUL
     </h4>
     <div class="frame">
-        <form >
+        <form method='post'>
             <br><br>
             <label for="company">Enter your company</label><br>
             <input class="for" type="text" id="company" name="company" required><br>
@@ -23,7 +40,7 @@
             <br>
             <label for="password">Enter your password</label><br>
             <input class="for"  type="password" id="password" name="password" required><br><br>
-            <button type="submit" class="submit1">Login</button><br><br>
+            <button type="submit" name="login" class="submit1">Login</button><br><br>
             <a href="forgot.html">forgot your password?</a>
             
     </div>
--- request/requestdemo.php
+++ request/requestdemo.php
@@ -1,4 +1,5 @@
 <?php include('connection.php'); ?>
+<?php include('login.php'); ?>
 <?PHP 
 if(!isset($_session)){
   session_start();
@@ -50,7 +51,7 @@ if(isset($_POST['submit'])){
               </div>
               <div class="form-group">
                 <label for="Mobile">Mobile:</label>
-                <input type="number" class="form-control" id="Mobile" placeholder="Enter Mobile" name="Mobile">
+                <input type="text" class="form-control" id="Mobile" placeholder="Enter Mobile" name="mobile">
               </div>
               <div class="form-group">
                 <label for="country">country:</label>
@@ -66,15 +67,15 @@ if(isset($_POST['submit'])){
               </div>
               <div class="form-group">
                 <label for="Industry">Industry:</label>
-                <input type="Industry" class="form-control" id="Industry" placeholder="Enter Industry" name="Industry">
+                <input type="text" class="form-control" id="Industry" placeholder="Enter Industry" name="industry">
               </div>
               <div class="form-group">
                 <label for="employees">No. of employees:</label>
-                <input type="employees" class="form-control" id="employees" placeholder="Enter employees" name="employees">
+                <input type="text" class="form-control" id="employees" placeholder="Enter employees" name="employees">
               </div>
               <div class="form-group">
                 <label for="Website">Website:</label>
-                <input type="Website" class="form-control" id="Website" placeholder="Enter Website" name="Website">
+                <input type="text" class="form-control" id="Website" placeholder="Enter Website" name="website">
               </div>
               <div class="checkbox">
                 <label><input type="checkbox" name="remember"> Remember me</label>
